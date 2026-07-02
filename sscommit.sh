1d95a4f89e9514eab5999c4532220cc52c93e5dc
Angel Casado <acasado@acm.org>
2026-07-02 09:27:14 +0200

    sync formulas

--- sscommit.sh
+++ sscommit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 scp -r aiserver:.claude/skills/check-club-handicaps/ skills/
 scp -r aiserver:.claude/skills/download-igc/ skills/
-scp -r aiserver:formulas/ formulas/
+scp -r aiserver:formulas/ .
 git add .
 git commit
+git push
